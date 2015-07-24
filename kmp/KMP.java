05a49b707c60de388f46c87055cd19c4a05122dd
lebi <15150692598@163.com>
2015-07-24 09:54:33 +0800

    kmp and BSearch

--- kmp/KMP.java
+++ kmp/KMP.java
@@ -1,8 +1,53 @@
 package kmp;
 
 public class KMP {
-	public String parent;
-	public String child;
+	public char[] parent;
+	public char[] child;
+	private int[] fail;
 	
+	public void failure(){
+		fail=new int[child.length];
+		fail[0]=-1;
+		for(int i=1;i<fail.length;i++){
+			int before=fail[i-1];
+			while(true){
+				if(child[before+1]==child[i]){
+					fail[i]=before+1;
+					break;
+				}else if(before==-1){
+					fail[i]=-1;
+					break;
+				}else 
+					before=fail[before];
+			}
+		}
+	}
 	
+	public int compare(){
+		failure();
+		int ci=0;
+		int pi=0;
+		while(true){
+			if(ci==child.length) return pi-ci;
+			if(pi==parent.length) return -1;
+			
+			if(parent[pi]==child[ci]){
+				pi++;
+				ci++;
+			}else{
+				ci=fail[ci];
+				ci++;
+				if(ci==0) pi++;
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		String p="abcmdacabacabcdabcmabcdbaadmcadma";
+		String c="abcdabcmabcd";
+		KMP kmp=new KMP();
+		kmp.parent=p.toCharArray();
+		kmp.child=c.toCharArray();
+		System.out.println(kmp.compare());
+	}
 }
