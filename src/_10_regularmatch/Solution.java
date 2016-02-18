6b5bf0b03c95ec3a4b1b4a4bcf2a294fc51b309e
lebi <15150692598@163.com>
2016-02-18 21:00:02 +0800

    reg

--- src/_10_regularmatch/Solution.java
+++ src/_10_regularmatch/Solution.java
@@ -2,11 +2,34 @@ package _10_regularmatch;
 
 public class Solution {
     public boolean isMatch(String s, String p) {
-        char before;
-        int j=0;
-        for(int i=0;i<s.length();i++){
-        	
-        }
-        return true;
+    	if(s.length()==0){
+    		if(p.length()%2!=0)
+    			return false;
+    		for(int i=1;i<p.length();i+=2)
+    			if(p.charAt(i)!='*')
+    				return false;
+    		return true;
+    	}
+    	
+    	if(p.length()==0)
+    		return false;
+    	
+    	if(p.length()==1)
+    		if(s.length()==1&&(s.charAt(0)==p.charAt(0)||p.charAt(0)=='.'))
+    			return true;
+    		else
+    			return false;
+    	if(p.charAt(1)!='*'){
+        	if(p.charAt(0)=='.'||p.charAt(0)==s.charAt(0))
+        		return isMatch(s.substring(1), p.substring(1));
+    	}
+    	if(p.charAt(0)=='.'||p.charAt(0)==s.charAt(0))
+    		if(isMatch(s, p.substring(2)))
+    			return true;
+    		else
+    			return isMatch(s.substring(1), p);
+    	else
+    		return isMatch(s, p.substring(2));
+    	
     }
 }
